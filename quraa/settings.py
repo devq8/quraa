8ebcd338a3611190fde4e4b4db80ae89e7271b15
Ubuntu <ubuntu@ip-172-31-3-96.eu-north-1.compute.internal>
2026-05-12 05:33:34 +0000

    Debug Static files

--- quraa/settings.py
+++ quraa/settings.py
@@ -183,6 +183,8 @@ else:
         },
     }
     MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com/'
+    STATIC_URL = '/static/'
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
